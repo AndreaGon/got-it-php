f9e230a7754b0d93c4291fb5eb68dbff7b872457
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2021-06-27 14:41:10 +0800

    Update lostitemform.php

--- lostitemform.php
+++ lostitemform.php
@@ -90,7 +90,6 @@
   echo '<main>';
   echo '<header class="row tm-welcome-section">';
   echo '<h2 class="col-12 text-center tm-section-title">Submit a Lost Item</h2>';
-  echo '<p class="col-12 text-center">Total 3 HTML pages are included in this template. Header image has a parallax effect. You can feel free to download, edit and use this TemplateMo layout for your commercial or non-commercial websites.</p>';
   echo '</header>';
   echo '';
   echo '';
